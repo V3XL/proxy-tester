f2f84d5978eb405e35c54aa675f46fb70655f8e1
V3XL <48788566+V3XL@users.noreply.github.com>
2024-08-10 21:58:57 +1000

    a few changes
    Now returns json results, including responsetime.
    Now adds logging to the console

--- Program.cs
+++ Program.cs
@@ -1,23 +1,55 @@
 using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string TestProxy(string host, string port, string proxyType){
-    HttpClient client = new HttpClient(new SocketsHttpHandler()
+JObject TestProxy(string host, string port, string proxyType)
+{
+    using (var handler = new SocketsHttpHandler
     {
-        Proxy = new WebProxy(proxyType + "://"+host+":"+port)
-    });
-    client.Timeout = TimeSpan.FromSeconds(10);
-    string content = null;
-    try{
-        if(client.GetStringAsync("http://ipinfo.io/").Result != null){
-            content = "{'status':'success'}";
+        Proxy = new WebProxy($"{proxyType}://{host}:{port}"),
+        UseProxy = true
+    })
+    using (var client = new HttpClient(handler))
+    {
+        client.Timeout = TimeSpan.FromSeconds(10);
+        string content = null;
+        long responseTime = 0;
+        bool isSuccess = false;
+        try
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            content = client.GetStringAsync("http://ifconfig.me/ip").GetAwaiter().GetResult();
+            stopwatch.Stop();
+            responseTime = stopwatch.ElapsedMilliseconds;
+
+            //Check that an IP is address string is returned by ifconfig.me
+            Regex r = new Regex(@"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$");
+            Match match = r.Match(content);
+
+            if(match.Success){
+                isSuccess = true;
+            }
+
         }
-    }catch (Exception){
-        content = "{'status':'error'}";
+        catch (Exception){}
+
+        JObject result = new JObject();
+        result.Add("successful", isSuccess);
+        result.Add("responseTime", responseTime);
+        
+        if(isSuccess){
+            Console.WriteLine($"[{host}:{port}] Type: {proxyType.ToUpper()} | Successful: {isSuccess} | ResponseTime: {responseTime}ms ");
+        }else{
+            Console.WriteLine($"[{host}:{port}] Type: {proxyType.ToUpper()} | Successful: {isSuccess}");
+        }
+        
+
+        return result;
     }
-    return content;
 }
 
 app.MapGet("/", async context =>
@@ -32,13 +64,15 @@ app.MapGet("/", async context =>
     await context.Response.WriteAsync(response);
 });
 
-app.MapGet("/socks", (string host, string port) =>
-    TestProxy(host, port, "socks")
-);
+app.MapGet("/socks", (string host, string port) => {
+    string jsonResult = TestProxy(host, port, "socks").ToString(Formatting.None);
+    return Results.Text(jsonResult, "application/json"); 
+});
 
+app.MapGet("/http", (string host, string port) => {
+    string jsonResult = TestProxy(host, port, "http").ToString(Formatting.None);
+    return Results.Text(jsonResult, "application/json"); 
+});
 
-app.MapGet("/http", (string host, string port) =>
-    TestProxy(host, port, "http")
-);
-
+app.Urls.Add("http://*:80");
 app.Run();
